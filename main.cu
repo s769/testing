bac7bca4cecc39985a24c4daeb5d07a2b9597fc1
s769 <s769.098211@gmail.com>
2023-11-16 20:55:03 -0500

    testing petsc vecnorm

---
+++ main.cu
@@ -0,0 +1,60 @@
+#include "shared.cuh"
+
+int main(int argc, char **argv) {
+
+
+    int world_rank, num_ranks;
+    int proc_rows, proc_cols;
+    int n;
+    PetscFunctionBeginUser;
+    PetscCall(PetscInitialize(&argc, &argv, NULL, NULL));
+    PetscCall(PetscOptionsGetInt(NULL, NULL, "-proc_rows", &proc_rows, NULL));
+    PetscCall(PetscOptionsGetInt(NULL, NULL, "-proc_cols", &proc_cols, NULL));
+    PetscCall(PetscOptionsGetInt(NULL, NULL, "-n", &n, NULL));
+
+
+    PetscCall(MPI_Comm_rank(MPI_COMM_WORLD, &world_rank));
+    PetscCall(MPI_Comm_size(MPI_COMM_WORLD, &num_ranks));
+
+    int row_rank = world_rank / proc_cols;
+    int col_rank = world_rank % proc_cols;
+    double *a, *d_a;
+
+    Vec v;
+    if (row_rank == 0)
+    {
+        a = (double *) malloc(n * sizeof(double));
+        for (int i = 0; i < n; i++) {
+            a[i] = i;
+        }
+        cudaMalloc(&d_a, n * sizeof(double));
+        cudaMemcpy(d_a, a, n * sizeof(double), cudaMemcpyHostToDevice);
+
+    }
+
+    PetscCall(VecCreate(PETSC_COMM_WORLD, v));
+    PetscLayout layout;
+    PetscCall(PetscLayoutCreate(PETSC_COMM_WORLD, &layout));
+    PetscCall(PetscLayoutSetBlockSize(layout, 1));
+    if (row_color == 0)
+        PetscCall(PetscLayoutSetLocalSize(layout, n/proc_cols));
+    else
+        PetscCall(PetscLayoutSetLocalSize(layout, 0));
+    PetscCall(PetscLayoutSetUp(layout));
+    PetscCall(VecSetLayout(*v, layout));
+    PetscCall(VecSetType(*v, VECCUDA));
+
+
+    if (color == 0)
+    {
+        PetscCall(VecCUDAReplaceArray(*v, d_a));
+    }
+    PetscCall(VecSetUp(*v));
+
+
+    
+
+    PetscCall(PetscFinalize());
+    return 0;
+
+}
---
+++ shared.cuh
@@ -0,0 +1,11 @@
+#ifndef __shared_h__
+#define __shared_h__
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+#include <petsc.h>
+#include <petscviewer.h>
+
+#endif
