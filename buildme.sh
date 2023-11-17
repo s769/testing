d82f5095bd23cbf7c75efd0ad83570899ed8cff8
s769 <s769.098211@gmail.com>
2023-11-16 21:35:04 -0500

    testing petsc vecnorm

--- buildme.sh
+++ buildme.sh
@@ -3,4 +3,4 @@ cmake .
 make -j
 
 # run with:
-# ibrun -np 3 ./test -proc_rows 3 -proc_cols 1 -n 10 -use_gpu_aware_mpi 0
+# mpiexec -np 3 ./test -proc_rows 3 -proc_cols 1 -n 10 -use_gpu_aware_mpi 0
