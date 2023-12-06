bfdc7bd3b851ae9a837e69b366bafc2f585dc53d
s769 <s769.098211@gmail.com>
2023-12-05 21:53:06 -0600

    testing petsc vecscatter reindex

--- buildme.sh
+++ buildme.sh
@@ -3,4 +3,4 @@ cmake . -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_CUDA_COMPIL
 make -j
 
 # run with:
-# mpiexec -np  ./test -proc_rows 3 -proc_cols 1 -n 10 -use_gpu_aware_mpi 0
+# mpiexec -np 12 ./test -proc_rows 4 -proc_cols 3 -nm 3 -nt 12 -use_gpu_aware_mpi 0
