9c7f240c3474d5e97931b7135dc4dcc63a933492
s769 <srvenkat@c301-003.ls6.tacc.utexas.edu>
2024-08-05 17:37:02 -0500

    matview test

--- main.cu
+++ main.cu
@@ -7,10 +7,13 @@ int main(int argc, char **argv) {
     Mat A;
     PetscViewer viewer;
     PetscInt N = 49000;
+    PetscMPIInt rank;
     PetscFunctionBeginUser;
     PetscCall(PetscInitialize(&argc, &argv, NULL, NULL));
     PetscCall(PetscOptionsGetInt(NULL, NULL, "-N", &N, NULL));
-    PetscCall(MatCreateDenseCUDA(PETSC_COMM_WORLD,PETSC_DECIDE,PETSC_DECIDE,N,N,NULL,&A));
+    PetscCallMPI(MPI_Comm_rank(PETSC_COMM_WORLD, &rank));
+    PetscInt loc_size = (rank == 0) ? N : 0;
+    PetscCall(MatCreateDenseCUDA(PETSC_COMM_WORLD,loc_size,loc_size,PETSC_DETERMINE,PETSC_DETERMINE,NULL,&A));
     PetscCall(PetscViewerBinaryOpen(PETSC_COMM_WORLD, "A.dat", FILE_MODE_WRITE, &viewer));
     PetscCall(PetscViewerPushFormat(viewer, PETSC_VIEWER_NATIVE));
     PetscCall(MatView(A, viewer));
